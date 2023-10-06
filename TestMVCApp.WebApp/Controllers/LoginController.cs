63bd208ba946f2b26a6a0725a8bbff49e63c2d3f
mmb <mmb02061990@gmail.com>
2023-10-06 19:37:36 +0530

    Add project files.

---
+++ TestMVCApp.WebApp/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using TestMVCApp.WebApp.Models;
+
+namespace TestMVCApp.WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            UserMST user = new UserMST();
+           // user.startDate = DateTime.Now;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserMST user) 
+        {
+            string connectionstr = ConfigurationManager.ConnectionStrings["myconnect"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connectionstr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = string.Format("Select * FROM UserMST WHERE userpassword = '{0}' and Email='{1}'", user.userpassword, user.email);
+                cmd.Connection = conn;
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    return View("Success");
+                }
+            }
+            return View("ErrorPage");
+        }
+    }
+}
---
+++ TestMVCApp.WebApp/Models/UserMST.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestMVCApp.WebApp.Models
+{
+    public class UserMST
+    {
+        [Display(Name = "User Name : <br>(This is the user name field)")]
+        [Required(ErrorMessage = "Please enter User Name.")]
+        public string email { get; set; }
+
+        [Display(Name = "User Password : <br>(This is password field where user can enter the password)")]
+        [Required(ErrorMessage = "Please enter User Password.")]
+        public string userpassword { get; set; }
+    }
+}
